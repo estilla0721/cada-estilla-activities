8b1c1b1518ff41516ba118707fbcf0a0f53304f2
unknown <cadaestillagean@gmail.com>
2026-02-11 17:26:06 -0800

    fetch

---
+++ fetch/js/fetch.js
@@ -0,0 +1,27 @@
+async function submitData(username, password) {
+   
+    const data = {
+        username: username,
+        password: password
+    };
+
+    const response =  await fetch(
+         'http://localhost/api/login.php',
+        {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(data)
+        }
+    );
+        
+   const resData = await reponse.json();
+   
+   console.log(resData);
+
+   if (!response.ok) {
+        throw new Error(resData.message || 'Login failed');
+   }
+   return resData;
+}
